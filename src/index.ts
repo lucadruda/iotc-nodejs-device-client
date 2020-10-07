c8cbc8c55f0fc050852bdf9a834d8514e1b373ae
lucadruda <ludruda@microsoft.com>
2020-10-07 18:04:04 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Luca Druda. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import { IIoTCClient } from './types/interfaces';
-import { IoTCClient } from './clients/iotcClient';
+
 import { CertificateGenerator } from './utils/certificates';
-
-
-export { IIoTCClient };
-export { IoTCClient };
 export { CertificateGenerator };
+
+export { IoTCClient } from './clients/iotcClient';
+export * from './types/constants';
+export * from './types/interfaces';
+export * from './utils/common';
